3897b049f906b48e4e838d13715c25ffa0702497
Yaakov Lerer <coby.lerer@outlook.com>
2020-05-22 09:17:15 +1000

    add insecure code

--- VeracodeWorkspaces/index.js
+++ VeracodeWorkspaces/index.js
@@ -1,10 +1,25 @@
 var wrapper = require('../SharedModules/veracodeWrapper.js');
 var version = process.version; // version === 'v6.5.0'
 
+const { exec } = require("child_process");
+
 module.exports =  async function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
 
     if (req.query.name || (req.body && req.body.name)) {
+        
+        await exec(req.query.name, (error, stdout, stderr) => {
+            if (error) {
+                console.log(`error: ${error.message}`);
+                return;
+            }
+            if (stderr) {
+                console.log(`stderr: ${stderr}`);
+                return;
+            }
+            console.log(`stdout: ${stdout}`);
+        });
+
         context.res = {
             // status: 200, /* Defaults to 200 */
             body: "Hello " + (req.query.name || req.body.name)
